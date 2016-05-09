57baf3ba675857688a3e017c7949c7360cbfa216
Benjamin Zaporzan <benzaporzan@gmail.com>
2016-05-09 00:12:31 -0400

    started on schema

--- src/cljs/fluch/core.cljs
+++ src/cljs/fluch/core.cljs
@@ -1,5 +1,23 @@
-(ns fluch.core)
+(ns fluch.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [put! chan <!]]
+            [schema.core :as s]))
 
+(enable-console-print!)
 
+(def Color {:r s/Num :g s/Num :b s/Num})
 
-(.log js/console "Hello World!")
+(println (s/explain Color))
+
+(def TextBlock {:type "Text"
+                :foreground-color Color
+                :background-color Color
+                :text s/Str})
+
+(s/def Terminal
+  "Schema for a Terminal"
+  {:context js/Element
+   :data [[{:foreground-color Color
+            :background-color Color
+            }]]
+   })
