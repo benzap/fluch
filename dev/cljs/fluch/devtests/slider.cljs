9c73fbe1156382de21a0ae02e5612c6681afce0c
Benjamin Zaporzan <benzaporzan@gmail.com>
2017-03-01 23:41:20 -0500

    good portion of the slider implementation is done

--- dev/cljs/fluch/devtests/slider.cljs
+++ dev/cljs/fluch/devtests/slider.cljs
@@ -10,21 +10,116 @@
 (instrument 'fluch.slider.clear
             'fluch.slider.create)
 
-(dc/deftest slider-create
-  (is-conformed ::slider/slider (slider/create))
-  (is-conformed ::slider/slider (slider/create "test")))
+(dc/deftest slider-tests
 
-(dc/deftest slider-clear
-  (is-conformed ::slider/slider (-> (slider/create "test") slider/clear)))
+  (t/testing "slider/create"
+    (is-conformed ::slider/slider (slider/create))
+    (is-conformed ::slider/slider (slider/create "test")))
 
-(dc/deftest slider-beginning
+  (t/testing "slider/clear"
+    (is-conformed ::slider/slider (-> (slider/create "test") slider/clear))
+    (t/is (let [sl (slider/create "test")
+                sl0 (slider/clear sl)]
+            (empty? (sl0 ::slider/after)))))
+
+  (t/testing "slider/beginning"
     (is-conformed
      ::slider/slider
      (-> (slider/create) slider/beginning)))
 
-(dc/deftest slider-end
-  (is-conformed 
-   ::slider/slider
-   (-> (slider/create)
-       slider/end)))
+  (t/testing "slider/end"
+    (is-conformed 
+     ::slider/slider
+     (-> (slider/create)
+         slider/end)))
 
+  (t/testing "slider/get-char"
+    (is-conformed
+     char?
+     (-> (slider/create "test")
+         slider/get-char))
+
+    (t/is (let [sl (slider/create "test")]
+            (= (slider/get-char sl) "t"))))
+
+  (t/testing "slider/left"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/left 2)))
+
+    (t/is (let [sl (-> (slider/create "test")
+                     (slider/end)
+                     (slider/left 2))
+                _ (println sl)]
+          (= (slider/get-char sl) "s"))))
+
+  (t/testing "slider/right"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/right 2)))
+
+    (t/is (let [sl (-> (slider/create "test")
+                     (slider/right 2))
+                _ (println sl)]
+          (= (slider/get-char sl) "s"))))  
+
+  (t/testing "slider/set-point"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/set-point 2)))
+
+    (t/is (let [sl (-> (slider/create "test")
+                       (slider/set-point 2))]
+            (= (slider/get-char sl) "s"))))
+
+  (t/testing "slider/insert"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/insert "ing")))
+    (t/is (let [sl (-> (slider/create "test")
+                       (slider/insert "ing")
+                       (slider/left 3))]
+            (= (slider/get-char sl) "i"))))
+  
+  (t/testing "slider/delete"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/end)
+         (slider/delete 2)))
+    (t/is (let [sl (-> (slider/create "test")
+                       (slider/end)
+                       (slider/delete 2)
+                       (slider/left 1))]
+            (= (slider/get-char sl) "e"))))
+
+  (t/testing "slider/set-mark slider/get-mark slider/remove-mark slider/clear-marks"
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/set-mark :test)))
+
+    (is-conformed
+     ::slider/point
+     (-> (slider/create "test")
+         (slider/set-point 2)
+         (slider/set-mark :test)
+         (slider/get-mark :test)))
+
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/set-mark :test)
+         (slider/remove-mark :test)))
+
+    (is-conformed
+     ::slider/slider
+     (-> (slider/create "test")
+         (slider/set-mark :test)
+         (slider/clear-marks))))
+  
+)
--- src/cljs/fluch/slider.cljs
+++ src/cljs/fluch/slider.cljs
@@ -6,13 +6,14 @@
 
 (defn lazyseq? [x] (instance? cljs.core/LazySeq x))
 (defn list-or-lazy? [x] (or (lazyseq? x) (list? x)))
+(defn string-or-keyword? [x] (or (string? x) (keyword? x)))
 
 (s/def ::before (s/coll-of char? :kind list-or-lazy? :into '()))
 (s/def ::after (s/coll-of char? :kind list-or-lazy? :into '()))
 (s/def ::point nat-int?)
 (s/def ::linenumber nat-int?)
 (s/def ::totallines nat-int?)
-(s/def ::marks (s/map-of string? nat-int?))
+(s/def ::marks (s/map-of string-or-keyword? nat-int?))
 
 (s/def ::slider 
   (s/keys :req-n [::before ::after ::point ::linenumber ::totallines ::marks]))
@@ -28,11 +29,16 @@
       ::marks {}}))
   ([] (create '())))
 
-(s/fdef create :ret ::slider)
+(s/fdef create
+        :args (s/alt :text (s/coll-of char? :kind list-or-lazy? :into '())
+                     :empty nil)
+        :ret ::slider)
 
 (defn clear [slider] (create))
 
-(s/fdef clear :ret ::slider)
+(s/fdef clear
+        :args (s/cat :slider ::slider)
+        :ret ::slider)
 
 (defn beginning [slider]
   (assoc slider
@@ -41,7 +47,9 @@
          ::point 0
          ::linenumber 1))
 
-(s/fdef beginning :ret ::slider)
+(s/fdef beginning 
+        :args (s/cat :slider ::slider)
+        :ret ::slider)
 
 (defn end [slider]
   (let [temp-content (concat (reverse (::after slider)) (::before slider))]
@@ -51,3 +59,156 @@
            ::point (count temp-content)
            ::linenumber (::totallines slider))))
 
+(s/fdef end
+        :args (s/cat :slider ::slider)
+        :ret ::slider)
+
+(defn get-char [slider]
+  (first (::after slider)))
+
+(s/fdef get-char
+        :args (s/cat :slider ::slider)
+        :ret char?)
+
+(defn get-point [slider]
+  (::point slider))
+
+(s/fdef get-point
+        :args (s/cat :slider ::slider)
+        :ret ::point)
+
+(defn get-linenumber [slider]
+  (::linenumber slider))
+
+(s/fdef get-linenumber
+        :args (s/cat :slider ::slider)
+        :ret ::linenumber)
+
+(defn slide-marks [marks point amount]
+  (let [ks (keys (select-keys marks (for [[k v] marks :when (> v point)] k)))]
+    (reduce #(assoc %1 %2 (max point (+ (%1 %2) amount))) marks ks)))
+
+(s/fdef slide-marks
+        :args (s/cat :marks ::marks :point ::point :amount nat-int?)
+        :ret ::marks)
+
+(defn left [slider amount]
+  (let [tmp (take amount (::before slider))
+        n (count tmp)
+        linecount (count (filter #(= % "\n") tmp))]
+    (assoc slider
+           ::before (drop amount (::before slider))
+           ::after (concat (reverse tmp) (::after slider))
+           ::point (- (::point slider) n)
+           ::linenumber (- (::linenumber slider) linecount))))
+
+(s/fdef left
+        :args (s/cat :slider ::slider :amount nat-int?)
+        :ret ::slider)
+
+(defn right [slider amount]
+  (let [tmp (take amount (::after slider))
+        n (count tmp)
+        linecount (count (filter #(= % "\n") tmp))]
+    (assoc slider
+           ::before (concat (reverse tmp) (::before slider))
+           ::after (drop n (::after slider))
+           ::point (+ (::point slider) n)
+           ::linenumber (+ (::linenumber slider) linecount))))
+
+(s/fdef right
+        :args (s/cat :slider ::slider :amount nat-int?)
+        :ret ::slider)
+
+(defn set-point [slider newpoint]
+  (cond
+    (> newpoint (::point slider))
+    (right slider (- newpoint (::point slider)))
+    (< newpoint (::point slider))
+    (left slider (- (::point slider) newpoint))
+    :else slider))
+
+(s/fdef set-point
+        :args (s/cat :slider ::slider
+                     :newpoint ::point)
+        :ret ::slider)
+
+(defn beginning? [slider]
+  (empty? (::before slider)))
+
+(s/fdef beginning?
+        :args (s/cat :slider ::slider)
+        :ret boolean?)
+
+(defn end? [slider]
+  (empty? (::after slider)))
+
+(s/fdef end?
+        :args (s/cat :slider ::slider)
+        :ret boolean?)
+
+(defn insert [slider text]
+  (let [n (count text)
+        linecount (count (filter #(= % "\n") text))]
+    (assoc slider
+           ::before (concat (reverse text) (::before slider))
+           ::point (+ (::point slider) n)
+           ::linenumber (+ (::linenumber slider) linecount)
+           ::totallines (+ (::totallines slider) linecount)
+           ::marks (slide-marks (::marks slider) (+ (::point slider) n -1) n))))
+
+(s/fdef insert
+        :args (s/cat :slider ::slider :text string?)
+        :ret ::slider)
+
+(defn delete [slider amount]
+  (let [tmp (take amount (::before slider))
+        linecount (count (filter #(= % "\n") tmp))
+        n (count tmp)]
+    (assoc slider
+           ::before (drop n (::before slider))
+           ::point (- (::point slider) n)
+           ::linenumber (- (::linenumber slider) linecount)
+           ::totallines (- (::totallines slider) linecount)
+           ::marks (slide-marks (::marks slider) (- (::point slider) n) n))))
+
+(s/fdef delete
+        :args (s/cat :slider ::slider :amount nat-int?)
+        :ret ::slider)
+
+(defn set-mark [slider name]
+  (assoc-in slider [::marks name] (::point slider)))
+
+(s/fdef set-mark
+        :args (s/cat :slider ::slider :name string-or-keyword?)
+        :ret ::slider)
+
+(defn get-mark [slider name]
+  (get-in slider [::marks name] nil))
+
+(s/fdef get-mark
+        :args (s/cat :slider ::slider :name string-or-keyword?)
+        :ret ::point)
+
+(defn remove-mark [slider name]
+  (update-in slider [::marks] dissoc name))
+
+(s/fdef remove-mark
+        :args (s/cat :slider ::slider :name string-or-keyword?)
+        :ret ::slider)
+
+(defn clear-marks [slider]
+  (assoc slider ::marks {}))
+
+(s/fdef clear-marks
+        :args (s/cat :slider ::slider)
+        :ret ::slider)
+
+(defn point-to-mark [slider name]
+  (if (get-mark slider name)
+    (set-point slider (get-mark slider name))
+    slider))
+
+(s/fdef point-to-mark
+        :args (s/cat :slider ::slider :name string-or-keyword?)
+        :ret ::slider)
